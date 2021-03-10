d6fa83a0b6d954218f8e285c18ba9a9851e3dc67
ido roseman <>
2021-03-10 16:38:33 +0200

    faster rendering of logbook

--- src/components/Logbook/index.js
+++ src/components/Logbook/index.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import clsx from 'clsx';
 import { compose } from 'recompose';
 
-import { Button } from '@material-ui/core';
-import Grid from '@material-ui/core/Grid';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 
@@ -11,64 +9,74 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import List from '@material-ui/core/List';
 import { FixedSizeList } from 'react-window';
+import AutoSizer from "react-virtualized-auto-sizer";
 
 import { withAuthorization } from '../Session';
-import { useStyles } from '../layout'
 import { withStyles } from '@material-ui/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
 import QsoTile from './qso_tile'
 
 var moment = require('moment');
 
+const useStyles = makeStyles({
+  root: {
+    height: "100%",
+    overflowX: "hidden",
+  },
+  
+  List: {
+    border: "1px solid #d9dddd"
+  },
+  logbook:{
+    backgroundColor: 'white',
+
+  },
+});
+
 const LogbookPage = (props) => {
 
+  const classes = useStyles();
+
+  const filtered_list = props.qsos.filter((item) => {
+    var isMatch = false;
+
+    ["CALL", "FREQ", "MODE", "NAME", "QTH", "GRID", "COMMENT", "MY_CITY"].forEach((fieldName) => {
+      if ((item[fieldName]) && (item[fieldName].toString().toLowerCase().search(props.filterText.toLowerCase()) !== -1))
+        isMatch = true;
+    })
+    if (moment.utc(item["timestamp_"]).locale("en-gb").format("L").toLowerCase().search(props.filterText.toLowerCase()) !== -1)
+      isMatch = true;
+    if ((item["country_"]) && (item["country_"].toLowerCase().includes(props.filterText.toLowerCase())))
+      isMatch = true;
+    return isMatch;
+  })
+
   const renderRow = (props) => {
-    const { index, style } = props;
-  
-    return (
-      <ListItem button style={style} key={index}>
-        <ListItemText primary={`Item ${index + 1}`} />
-      </ListItem>
-    );
+    const { key, index, style } = props;
+
+    return props.loading
+      ? <Typography component="h2" variant="h6" color="primary" gutterBottom>Loading... </Typography>
+      : <div key={key} style={style}>
+          <QsoTile key={key} style={style} qso={filtered_list[index]} />
+        </div>
   }
 
-  const { classes } = this.props;
-    
-  const filtered_list = this.props.qsos.filter((item)=>{
-      var isMatch = false;
-
-      ["CALL", "FREQ", "MODE", "NAME", "QTH", "GRID", "COMMENT", "MY_CITY"].forEach((fieldName)=>{
-        if ((item[fieldName]) && (item[fieldName].toString().toLowerCase().search(this.props.filterText.toLowerCase()) !== -1))
-          isMatch= true;        
-      })
-      if (moment.utc(item["timestamp_"]).locale("en-gb").format("L").toLowerCase().search(this.props.filterText.toLowerCase()) !== -1)
-        isMatch= true;
-      if ((item["country_"]) && (item["country_"].toLowerCase().includes(this.props.filterText.toLowerCase())))
-        isMatch= true;
-      return isMatch;
-    })
-
-  var qso_list = []
-  filtered_list.forEach((qso) => {
-    const ukey = qso.QSO_DATE+"-"+qso.TIME_ON+"-"+qso.CALL;
-    qso_list.push(<QsoTile key={ukey} qso={qso}/>)
-  });
-  if (qso_list.length==0)
-    qso_list.push(<Typography variant="body2" component="p">
-    No QSOs to show
-  </Typography>)
-
   return (
-        <Paper className={clsx(classes.paper,classes.logbook)}>
-        {this.props.loading ? <div>Loading ...</div> : <>{qso_list}</>}
 
-        <FixedSizeList height={400} width={300} itemSize={46} itemCount={200}>
-          {renderRow}
-        </FixedSizeList>
-        </Paper>
+    <Paper className={clsx(classes.paper, classes.logbook)}>
+      <div style={{ height: '80vh' }}>
+        <AutoSizer>
+          {({ height, width }) => (
+            <FixedSizeList height={height} width={width} itemSize={125} itemCount={filtered_list.length} >
+              {renderRow}
+            </FixedSizeList>
+          )}
+        </AutoSizer>
+      </div>
+    </Paper>
+  );
 
-    );
-  
 }
 
 const condition = authUser => !!authUser;
