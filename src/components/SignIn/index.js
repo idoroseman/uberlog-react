06a071ca8aaca3e62afe05c4f31a2b3fa02c6bc9
ido roseman <>
2021-03-19 10:26:21 +0200

    nice looking sign in page

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -8,12 +8,44 @@ import { PasswordForgetLink } from '../PasswordForget';
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../constants/routes';
 
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
 const SignInPage = () => (
   <div>
     <h1>SignIn</h1>
     <SignInForm />
-    <PasswordForgetLink />
-    <SignUpLink />
   </div>
 );
 
@@ -23,62 +55,96 @@ const INITIAL_STATE = {
   error: null,
 };
 
-class SignInFormBase extends Component {
-  constructor(props) {
-    super(props);
+const SignInFormBase = (props) => {
 
-    this.state = { ...INITIAL_STATE };
-  }
+  const [state, setState] = React.useState(INITIAL_STATE);
+  const classes = useStyles();
 
-  onSubmit = event => {
-    const { email, password } = this.state;
-
-    this.props.firebase
+  const onSubmit = event => {
+    const { email, password } = state;
+    props.firebase
       .doSignInWithEmailAndPassword(email, password)
       .then(() => {
-        this.setState({ ...INITIAL_STATE });
-        this.props.history.push(ROUTES.HOME);
+        setState({ ...INITIAL_STATE });
+        props.history.push(ROUTES.HOME);
       })
       .catch(error => {
-        this.setState({ error });
+        setState({ error });
       });
 
     event.preventDefault();
   };
 
-  onChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
+  const onChange = event => {
+    setState({...state,  [event.target.name]: event.target.value });
   };
 
-  render() {
-    const { email, password, error } = this.state;
+  const { email, password, error } = state;
 
-    const isInvalid = password === '' || email === '';
-
-    return (
-      <form onSubmit={this.onSubmit}>
-        <input
+  const isInvalid = password === '' || email === '';
+  return (
+    <Container component="main" maxWidth="xs">
+    <div className={classes.paper}>
+      <Avatar className={classes.avatar}>
+        <LockOutlinedIcon />
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        Sign in
+      </Typography>
+      <form className={classes.form} noValidate onSubmit={onSubmit}>
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email Address"
           name="email"
+          autoComplete="email"
+          autoFocus
           value={email}
-          onChange={this.onChange}
-          type="text"
-          placeholder="Email Address"
+          onChange={onChange}
         />
-        <input
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
           name="password"
-          value={password}
-          onChange={this.onChange}
+          label="Password"
           type="password"
-          placeholder="Password"
+          id="password"
+          autoComplete="current-password"
+          value={password}
+          onChange={onChange}
         />
-        <button disabled={isInvalid} type="submit">
+        <FormControlLabel
+          control={<Checkbox value="remember" color="primary" />}
+          label="Remember me"
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          className={classes.submit}
+        >
           Sign In
-        </button>
-
-        {error && <p>{error.message}</p>}
+        </Button>
+        <Grid container>
+          <Grid item xs>
+            <PasswordForgetLink />
+          </Grid>
+          <Grid item>
+            <SignUpLink />
+          </Grid>
+        </Grid>
       </form>
-    );
-  }
+    </div>
+  </Container>
+
+  );
+  
 }
 
 const SignInForm = compose(
